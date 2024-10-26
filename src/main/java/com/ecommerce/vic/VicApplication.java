3f3486032a93210cc00c5c83a81977df2f8a75d4
0xDVC <neilohene@gmail.com>
2024-10-26 01:35:56 +0000

    init: setting up repo

---
+++ src/main/java/com/ecommerce/vic/VicApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.vic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/vic/VicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.vic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
